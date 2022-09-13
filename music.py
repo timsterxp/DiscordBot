2566b0c272a8d3b0d1ae40765c77b749c270fe69
Tim Nguyen <timmyn97@gmail.com>
2022-09-12 23:04:54 -0700

    Extra brackets

--- music.py
+++ music.py
@@ -346,7 +346,7 @@ class Music(commands.Cog):
 
         source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop, download=False)
 
-        if (vc.is_playing() or not player.queue.empty()):
+        if vc.is_playing() or not player.queue.empty():
             await player.queue.put(source)
         else:    
             vc.play(discord.FFmpegPCMAudio(source,**cls.ffmpeg_options), data=data, requester=ctx.author)
