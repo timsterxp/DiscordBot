9a98be95419ba2b28dc6cffc1aa69ba1b634f8e6
Tim Nguyen <timmyn97@gmail.com>
2022-09-12 23:17:58 -0700

    Bug fixes

--- music.py
+++ music.py
@@ -346,11 +346,11 @@ class Music(commands.Cog):
 
         source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop, download=False)
 
-        if vc.is_playing() or not player.queue.empty():
-       
+        if not vc.is_playing() and player.queue.empty():
+            return vc.play(discord.FFmpegPCMAudio(source,**cls.ffmpeg_options), data=data, requester=ctx.author)
+            
+        elif not player.queue.empty():
             return await player.queue.put(source)
-        else:    
-            vc.play(discord.FFmpegPCMAudio(source,**cls.ffmpeg_options), data=data, requester=ctx.author)
         
         if 'playlist?list' in search:
             #firstSong = await YTDLSource.playlist_start(ctx, search,loop=self.bot.loop,download=False)
